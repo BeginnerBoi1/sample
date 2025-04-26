a10a46fecd06e5bce1cafc1aeca874d2d2a1a668
Jan Randolph Emmanuel <103882365+BeginnerBoi1@users.noreply.github.com>
2025-04-27 00:38:06 +0800

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,17 @@
+class Person:
+    race = 'Filipino' # This is a class attribute
+                      # shared among objects
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age              # This is an instance attribute
+        self.gender = gender
+    
+    def introduce(self):
+        print(f"Hello im {self.name} and I'm {self.age} years old")
+    
+# WTF is a self?
+student = Person("Jan Randolph", 15, "Male")
+print(student.race)
+student.introduce()
+
